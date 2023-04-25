601da85c364cda1fa3930d5abf16b059d588f789
elijah <elijahchoi1030@gmail.com>
2023-04-25 11:16:41 +0900

    hw3 development

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,19 +1,21 @@
 import gym
 import numpy as np
-from hw3_utils import chooseAction, actionValue, showScreen
+from hw3_utils import featureVector, chooseAction, actionValue
 
 NUM_TILE = 8
-NUM_EPI = 1
+NUM_EPI = 500
 EPSILON = 0.1
-ALPHA = 0.1/8
+ALPHA = 0.01/8
 GAMMA = 1
 
 # render_mode = "rgb_array" : rgb array를 return하며 이를 plot할 수 있음. 빠름.
 # render_mode = "human"     : 자동으로 render()가 실행되어 pygame으로 보여줌. 느림. 
 env = gym.make('MountainCar-v0', render_mode="human")
-env.metadata["render_fps"] = 1000
+env.metadata["render_fps"] = 120
 
-weight = np.ones(NUM_TILE*2*env.action_space.n)
+# weight = np.zeros(NUM_TILE*2*env.action_space.n)
+# weight = np.zeros(NUM_TILE*2+1)
+weight = np.zeros(NUM_TILE*2)
 steps_per_epi = []
 
 for _ in range(NUM_EPI):
@@ -23,18 +25,27 @@ for _ in range(NUM_EPI):
     action = chooseAction(state, weight, env, epsilon=EPSILON)
     while True:
         next_state, reward, terminal, _, _ = env.step(action)
+        
+        if num_steps%300 == 2:
+            print(weight)
+            print(featureVector(state, action))
+            print(actionValue(next_state, next_action, weight))
+            print(actionValue(state, action, weight))
+            print(ALPHA*(reward + GAMMA*actionValue(next_state, next_action, weight) \
+                            - actionValue(state, action, weight))*featureVector(state, action))
+            print("CUT", num_steps, '\n')
+            # break
+        
         if terminal:
             # Assuming terminal reward is 0
-            weight += ALPHA*( - actionValue(state, action, weight))*weight
+            weight += ALPHA*(0 - actionValue(state, action, weight))*featureVector(state, action)
             break
         next_action = chooseAction(next_state, weight, env, epsilon=EPSILON)
         weight += ALPHA*(reward + GAMMA*actionValue(next_state, next_action, weight) \
-                        - actionValue(state, action, weight))*weight
-        state = next_state.copy()
-        action = next_action
+                        - actionValue(state, action, weight))*featureVector(state, action)
+        state, action = next_state, next_action
         num_steps += 1
-        print(num_steps)
-        # break
+        
     steps_per_epi.append(num_steps)
 
 env.close()
--- hw3/hw3_utils.py
+++ hw3/hw3_utils.py
@@ -1,9 +1,8 @@
-import gym
 import random
 import numpy as np
 import pygame
 
-def tileCoding(obs, num_tile, place_bound=(-1.2, 0.5), speed_bound=(-0.07, 0.07)):
+def featureVector(obs, action, num_tile=8, num_act=3, place_bound=(-1.2, 0.5), speed_bound=(-0.07, 0.07)):
     '''
         Tile coding: Check the textbook page 217.
         Using 8 tiles divided by 8 of each dimension, boundary is divided by 57 pieces in each dimension.
@@ -12,7 +11,6 @@ def tileCoding(obs, num_tile, place_bound=(-1.2, 0.5), speed_bound=(-0.07, 0.07)
     '''
     plc_offset = (place_bound[1]-place_bound[0])/(num_tile*(num_tile-1)+1)
     spd_offset = (speed_bound[1]-speed_bound[0])/(num_tile*(num_tile-1)+1)
-
     state_vector = []
     for i in range(num_tile):
         projected_place = (obs[0] - (place_bound[0] + (1-num_tile+i)*plc_offset))/(plc_offset*num_tile)
@@ -22,11 +20,8 @@ def tileCoding(obs, num_tile, place_bound=(-1.2, 0.5), speed_bound=(-0.07, 0.07)
         if projected_speed == num_tile:
             state_vector.append(num_tile-1)
         else:
-            state_vector.append(projected_speed)
-    return state_vector
-
-
-def actionValue(state, action, W, num_tile=8, num_act=3):
+            state_vector.append(projected_speed//1)
+    
     '''
         The state vector is created by tile coding.
         The feature vector has 3 times dimension of the state vector, each region only used for each actions.
@@ -34,12 +29,17 @@ def actionValue(state, action, W, num_tile=8, num_act=3):
                         action 1: [0, 0, 2, 3, 0, 0]
                         action 2: [0, 0, 0, 0, 2, 3]
     '''
-    state_vector = tileCoding(state, num_tile)
-    feature_vector = np.zeros(len(state_vector)*num_act)
-    feature_vector[action*len(state_vector):(action+1)*len(state_vector)] = state_vector
-    if feature_vector.shape != (48,):
-        print("SE")
-    return np.dot(W, feature_vector)
+    # feature_vector = np.zeros(len(state_vector)*num_act)
+    # feature_vector[action*len(state_vector):(action+1)*len(state_vector)] = state_vector
+    
+    # feature_vector = np.append(state_vector, action)
+    
+    feature_vector = np.array(state_vector) + np.ones(len(state_vector))*num_tile*action
+    return feature_vector
+
+
+def actionValue(state, action, W):
+    return np.dot(W, featureVector(state, action))
 
 
 def chooseAction(state, W, env, epsilon=0.1):
